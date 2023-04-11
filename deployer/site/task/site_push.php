542653c47d2f9a4667d65fdf63a6ccca8c4c6a19
Jimmy Andersson <jimmy@digitalera.se>
2023-04-11 11:09:35 +0200

    Cleaned up comments

--- deployer/site/task/site_push.php
+++ deployer/site/task/site_push.php
@@ -6,32 +6,6 @@ use Throwable;
 
 task('site:push', function () {
     writeln('Running site:push');
-    // $is_failed = false;
-    // try {
-    //     invoke('deploy');
-    // } catch (Throwable $th) {
-    //     $is_failed = true;
-    //     writeln($th->getMessage());
-    //     writeln('<error>The command deploy failed</error>');
-    // }
-    // if (!$is_failed) {
-    //     try {
-    //         invoke('db:push');
-    //     } catch (Throwable $th) {
-    //         $is_failed = true;
-    //         writeln($th->getMessage());
-    //         writeln('<error>The command db:push failed</error>');
-    //     }
-    // }
-    // if (!$is_failed) {
-    //     try {
-    //         invoke('media:push');
-    //     } catch (Throwable $th) {
-    //         $is_failed = true;
-    //         writeln($th->getMessage());
-    //         writeln('<error>The command media:push failed</error>');
-    //     }
-    // }
     invoke('deploy');
     invoke('db:push');
     invoke('media:push');
